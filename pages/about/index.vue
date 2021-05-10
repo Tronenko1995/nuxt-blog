11481bbf0402f31f56f3c1287b35c47306f4060a
alextr999 <tronenko.aleksandr@gmail.com>
2021-05-10 22:01:08 +0300

    initial nuxt project

---
+++ pages/about/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="wrapper-content wrapper-content--fixed">
+      <section class="about">
+          <div class="container">
+                <h1 class="title"> About my blog: </h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse aspernatur, obcaecati, voluptatum fugit molestias at, deserunt nulla cumque cupiditate maxime dolores atque repellat placeat et? Totam eligendi modi fuga doloribus aut quaerat et eveniet repellat ea. Qui at laboriosam maxime alias sunt modi atque magni doloribus obcaecati.</p>
+                <no-ssr>
+                    <Intro title="Qui at laboriosam"/>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse aspernatur, obcaecati, voluptatum fugit molestias at, deserunt nulla cumque cupiditate maxime dolores atque repellat placeat et? Totam eligendi modi fuga doloribus aut quaerat et eveniet repellat ea. Qui at laboriosam maxime alias sunt modi atque magni doloribus obcaecati.</p>
+                </no-ssr>
+                <img src="@/assets/img/about.jpg" alt="">
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse aspernatur, obcaecati, voluptatum fugit molestias at, deserunt nulla cumque cupiditate maxime dolores atque repellat placeat et? Totam eligendi modi fuga doloribus aut quaerat et eveniet repellat ea. Qui at laboriosam maxime alias sunt modi atque magni doloribus obcaecati.</p>
+          </div>
+      </section>
+  </div>
+</template>
+
+<script>
+import Intro from '@/components/UI/Intro.vue'
+export default {
+    components: {
+        Intro
+    }
+}
+</script>
+
+<style lang="scss">
+.about {
+    text-align: center;
+    p {
+        margin-bottom: 10px;
+    }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="wrapper-content wrapper-content--fixed">
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
